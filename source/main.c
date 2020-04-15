8046b8478f785059e98b475383110f4d04721d63
Bhagpreet Brar <32920058+Bhagpreet@users.noreply.github.com>
2020-04-14 17:11:35 -0700

    updated main.c to contain lab2part1 solution code

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
-/*	Author: bbrar002 Bhagpreet Brar
+/*	Author: Bhagpreet Brar bbrar 002
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab # 2 Exercise # 1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -13,14 +13,20 @@
 #endif
 
 int main(void) {
-	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs
-				// Initialize output on PORTB to 0x00
-	unsigned char temp_input = 0x00;
+	DDRA = 0x00; PORTA = 0xFF; 
+	DDRB = 0xFF; PORTB = 0x00; 
+							
+	unsigned char temp1 = 0x00;
+	unsigned char temp2 = 0x00;
+	
 	while(1) {
-		temp_input = PINA;
-		PORTB = temp_input;	// Writes port B's 8 pins with the values
-					//  on port A's 8 pins
+		temp1 = PINA & 0x01;
+		temp2 = PINA & 0x02;
+		if((temp1 == 0x01) && (temp2 == 0x00)) {
+			PORTB = 0x01;
+		}else {
+			PORTB = 0x00;
+		}
 	}
-	return 1;
+	return 0;
 }
