c967f9d99854c399640d4c30dd90d0eb6a7d93c5
Bhagpreet Brar <32920058+Bhagpreet@users.noreply.github.com>
2020-04-14 19:45:32 -0700

    added lab 2 part 4

--- source/main.c
+++ source/main.c
@@ -13,27 +13,32 @@
 #endif
 
 int main(void) {
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRC = 0xFF; PORTC = 0x00;
+	DDRA = 0x00; PORTA = 0x07;
+	DDRB = 0x00; PORTB = 0x0B;
+	DDRC = 0x00; PORTC = 0x0A;
+	DDRD = 0xFF; PORTD = 0x00;
 	
-	unsigned char cntavail = 0x00;
-	unsigned char spot1 = 0x00;
-	unsigned char spot2 = 0x00;
-	unsigned char spot3 = 0x00;
-	unsigned char spot4 = 0x00;
+	
+	
+	unsigned char a = 0x00;
+	unsigned char b = 0x00;
+	unsigned char c = 0x00;
+	unsigned char d = 0x00;
+	unsigned char tot = 0x00;
 	
     while(1) {
-		spot1 = PINA & 0x01;
-		spot2 = (PINA & 0x02) >> 1;
-		spot3 = (PINA & 0x04) >> 2;
-		spot4 = (PINA & 0x08) >> 3;
-		
-		cntavail = spot1 + spot2 + spot3 + spot4; // counts the spots that are occupied	
-		if (cntavail == 0x04){
-			PORTC = 0x80;
-		} else {
-			cntavail = 0x04 - cntavail; // subtract to get number of spots available	
-			PORTC = cntavail;
-		}
+	    a = PINA;
+	    b = PINB;
+	    c = PINC;
+	    tot = a + b + c;
+
+	    if (tot > 0x8C){
+		d = 0x0;
+	    } 
+	    if (abs(a - c) > 0x50){
+		d = d | 0x02;
+	    }
+	    PORTD = tot | d;
+	    d = 0;
     }
 }
